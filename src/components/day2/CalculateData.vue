c30f68486b651058f1fe4e3952ab626fbb9b8ee1
Anh Khoa DINH <anhkhoa.dinh@officience.com>
2023-06-06 23:54:39 +0700

    Day 2

---
+++ src/components/day2/CalculateData.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="calculate-data">
+    <label>
+      <button @click="sendResult()" >Calculate</button>
+    </label>
+    <div>
+      Result: {{ result }}
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return {
+        result: null
+      }
+    },
+    props: ['firstNumber', 'secondNumber', 'calculateType'],
+    emits: ["result"],
+    methods: {
+      sendResult () {
+        this.result = this.calculate
+      }
+    },
+    computed: {
+      calculate() {
+        if (this.calculateType == 'plus') {
+          return parseInt(this.firstNumber) + parseInt(this.secondNumber);
+        }else if (this.calculateType == 'sub'){
+          return parseInt(this.firstNumber) + parseInt(this.secondNumber);
+        }else if (this.calculateType == 'mul') {
+          return parseInt(this.firstNumber) * parseInt(this.secondNumber);
+        }else{
+          return 'Something is wrong!!!'
+        }
+      },
+    }
+  }
+</script>
---
+++ src/components/day2/Day2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="day-two">
+    <InputData @data_cal="onNewData($event)" />
+    <CalculateData v-bind="data_receive" @result="onNewResult($event)"/>
+  </div>
+</template>
+<script>
+import InputData from './InputData.vue'
+import CalculateData from './CalculateData.vue'
+export default{
+    components:{
+        InputData,
+        CalculateData
+    },
+  data() {
+    return {
+      data_receive: {
+        firstNumber: 0,
+        secondNumber: 0,
+        calculateType: null
+      },
+      result_receive: null
+    }
+  },
+  methods: {
+    onNewData(data_cal){
+      this.data_receive.firstNumber = data_cal[0].firstNumber;
+      this.data_receive.secondNumber = data_cal[1].secondNumber;
+      this.data_receive.calculateType = data_cal[2].calculateType;
+    },
+    onNewResult(result) {
+      this.result_receive = result;
+    }
+  }
+}
+</script>
